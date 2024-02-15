ba7eb2a6d54b0a74534a39e0714dc8a2b7b0aa75
Nik <niki.salli@gmail.com>
2024-02-15 16:40:07 +0100

    fix

--- generate_zip.sh
+++ generate_zip.sh
@@ -4,5 +4,5 @@ rm NikFEMM.zip
 mv metadata.json metadata_.json
 jq --arg today "$(date +%Y-%m-%d)" '.versions[0].version |= $today' metadata_.json > metadata.json
 
-git ls-files  -- 'python-kicad-plugin/metadata.json' 'python-kicad-plugin/resources*.png' 'python-kicad-plugin/plugins*.png' 'python-kicad-plugin/plugins*.py' | xargs zip NikFEMM.zip
+git ls-files  -- 'metadata.json' 'resources*.png' 'plugins*.png' 'plugins*.py' | xargs zip NikFEMM.zip
 mv metadata_.json metadata.json
